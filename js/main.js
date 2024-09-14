08830a1f0cc7c3763d2cec0b567b6fa18d387af6
threeant <threeant@foxmail.com>
2024-09-14 21:06:29 +0800

    网站样式更换

--- js/main.js
+++ js/main.js
@@ -1,65 +1,88 @@
-(function ($) {
-    "use strict";
+$(function() {
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
+  var nav_offset_top = $('header').height() + 50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
     };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+    navbarFixed();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
-        }
-    });
+
+    /*-------------------------------------------------------------------------------
+	  clients logo slider
+	-------------------------------------------------------------------------------*/
+    if ($('.clients_slider').length) {
+      $('.clients_slider').owlCarousel({
+          loop: true,
+          margin: 30,
+          items: 5,
+          nav: false,
+          dots: false,
+          responsiveClass: true,
+          autoplay: 2500,
+          slideSpeed: 300,
+          paginationSpeed: 500,
+          responsive: {
+              0: {
+                  items: 1,
+              },
+              768: {
+                  items: 3,
+              },
+              1024: {
+                  items: 4,
+              },
+              1224: {
+                  items: 5
+              }
+          }
+      })
+    }
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    /*-------------------------------------------------------------------------------
+	  testimonial slider
+	-------------------------------------------------------------------------------*/
+    if ($('.testimonial').length) {
+      $('.testimonial').owlCarousel({
+          loop: true,
+          margin: 30,
+          items: 5,
+          nav: false,
+          dots: true,
+          responsiveClass: true,
+          slideSpeed: 300,
+          paginationSpeed: 500,
+          responsive: {
+              0: {
+                  items: 1
+              }
+          }
+      })
+    }
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: true,
-        loop: true,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-    
-})(jQuery);
+  /*-------------------------------------------------------------------------------
+	  Mailchimp 
+	-------------------------------------------------------------------------------*/
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+  mailChimp();
+  
+});
+
 
